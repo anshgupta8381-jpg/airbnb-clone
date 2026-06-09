b55ba45136408671f60374643c9ba65790086292
Ansh Gupta <anshgupta8381@gmail.com>
2026-06-09 18:59:21 +0530

    fix: https callback URL

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.CALLBACK_URL || "/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
