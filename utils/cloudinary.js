4ae6a2d3a8be3f362a10b0d700c83ae11d84308e
Ansh Gupta <anshgupta8381@gmail.com>
2026-06-10 17:22:00 +0530

    fix: handling type

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -11,19 +11,6 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'airbnb-clone',
-    allowed_formats: ['jpgconst cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'airbnb-clone',
     allowed_formats: ['jpg', 'jpeg', 'png'],
   }
 });
@@ -40,8 +27,4 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-module.exports = { cloudinary, storage, fileFilter };', 'jpeg', 'png'],
-  }
-});
-
-module.exports = { cloudinary, storage };
+module.exports = { cloudinary, storage, fileFilter };
